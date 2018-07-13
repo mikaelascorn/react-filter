287db8905300c49ec7775276c94cb76b5146182c
Mikaela Scornaienchi <scornaim@gmail.com>
2018-07-13 02:39:14 -0400

    almost there

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -10,12 +10,13 @@ class App extends React.Component {
       search: "",
       data: [],
       displayData: [],
-      postData: []
+      postData: ""
     };
     this.updateSearch = this.updateSearch.bind(this);
     this.dataSearch = this.dataSearch.bind(this);
     this.dataSearch = this.dataSearch.bind(this);
     this.post = this.post.bind(this);
+    // this.highlight = this.highlight.bind(this);
   }
 
   componentDidMount() {
@@ -25,51 +26,61 @@ class App extends React.Component {
     });
   }
 
-  //this will register if the user selected the name and display 
+  //this will register if the user selected a name and display from the search
   post(e) {
     e.preventDefault();
     e.stopPropagation();
-    // console.log();
-    console.log(e.target);
 
-    console.log(e.target.txt);
-
-    const holdingArray = []
+    const buttonArray = [];
     const name = e.target;
-    for (let key in name) {
-      holdingArray.push(name)
-    }
-    console.log(holdingArray);
-    let index = Object.keys(holdingArray).indexOf(name);
-    console.log(index);
-    
-
+    buttonArray.push(name);
+    const filtering = buttonArray[0].textContent;
+    // console.log(filtering);
+    this.setState({
+      postData: filtering
+    });
   }
 
+  // highlight(wordToMatch) {
+  //   console.log(wordToMatch);
+  //   // const words = {
+  //   //   firstP: wordToMatch.firstname,
+  //   //   lastP: wordToMatch.lastname;,
+  //   // }
+  //   const firstP = wordToMatch.firstname;
+  //   const lastP = wordToMatch.lastname;
+  //         // const regex = new RegExp(wordToMatch, "gi");
 
+  //   return wordToMatch.firstname.filter(name => {
+  //   //   // here we need to figure out if the city or state matches what was searched
+  //     const regex = new RegExp(firstP, "gi");
+  //     return wordToMatch.firstname.match(regex));
+  //   });
+  // }
 
-  // this will hold the matches update state/clear with users keystrokes, combining first and last names
+  // this will hold the matches update state/clear users keystrokes, combining first and last names
   dataSearch(firstSearch, lastSearch) {
     // console.log(dataToSearch);
     let firstFiltered = firstSearch.map(data => {
       return data;
     });
-     let lastFiltered = lastSearch.map(data => {
-       return data;
-     });
+    let lastFiltered = lastSearch.map(data => {
+      return data;
+    });
 
     let personsFilter = lastFiltered.concat(firstFiltered);
-    // console.log(personsFilter);
-    
-    if (this.state.search === '' ) {
+  
+    if (this.state.search === "") {
       this.setState({
-      displayData: []
-    });
+        displayData: []
+      });
     } else {
       this.setState({
-      displayData: personsFilter
-    });
+        displayData: personsFilter
+      });
     }
+    this.highlight(personsFilter);
+
   }
 
   // get the value of the user keystroke, hold in state
@@ -77,48 +88,63 @@ class App extends React.Component {
   updateSearch(e) {
     const userInput = e.target.value.substr(0, 30);
 
-    this.setState({
-      search: userInput
-    }, () => {
-      let lastNameSearch = this.state.data.filter(user => {
-        return user.lastname
-          .toLowerCase()
-          .indexOf(this.state.search) !== -1;
-      });
-      let firstNameSearch = this.state.data.filter(user => {
-        return user.firstname
-          .toLowerCase()
-          .indexOf(this.state.search) !== -1;
-      });
-      this.dataSearch(lastNameSearch, firstNameSearch);
-    });
+    this.setState(
+      {
+        search: userInput
+      },
+      () => {
+        let lastNameSearch = this.state.data.filter(user => {
+          return user.lastname.toLowerCase().indexOf(this.state.search) !== -1;
+        });
+        let firstNameSearch = this.state.data.filter(user => {
+          return user.firstname.toLowerCase().indexOf(this.state.search) !== -1;
+        });
+        this.dataSearch(lastNameSearch, firstNameSearch);
+      }
+    );
   }
 
-
-  //  <ul>
-  //           {matches.map((people, i) => <li key={i}>
-  //             <button onClick={this.post}>{people.firstname} {people.lastname}</button>
-  //           </li>)}
-  //         </ul>
-
   render() {
+    // const cityName = place.city.replace(regex, );
+
     let matches = this.state.displayData;
-    return <div className="renderWrapper">
+    return (
+      <div className="renderWrapper">
         <h1>Search It!</h1>
         <div className="search">
           <label>Search a name!</label>
-          <input type="text" placeholder="Type a name" value={this.state.search} onChange={this.updateSearch} />
+          {this.state.postData === "" ? (
+            <input
+              ref="searchInput"
+              id="input"
+              type="text"
+              placeholder="Start searching"
+              value={this.state.search}
+              onChange={this.updateSearch}
+            />
+          ) : (
+            <input
+              id="input"
+              type="text"
+              placeholder="Start searching"
+              value={this.state.postData}
+              onChange={this.updateSearch}
+            />
+          )}
         </div>
         <div className="results">
           <ul>
-            {matches.map((people, i) => <li key={i} onClick={this.post}>
+            {matches.map((people, i) => (
+              <li key={i} onClick={this.post}>
                 <button>
                   {people.firstname} {people.lastname}
                 </button>
-              </li>)}
+              </li>
+            ))}
           </ul>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
--- dev/server/index.js
+++ dev/server/index.js
@@ -8,7 +8,7 @@ const createUser = () => {
 };
 
 // Try changing this to test your input!
-const USER_COUNT = 1000;
+const USER_COUNT = 100;
 
 const users = Array(USER_COUNT)
   .fill("")
