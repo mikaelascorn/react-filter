d293916e39e003141b3123c3f70bf0f55a393cf2
Mikaela Scornaienchi <scornaim@gmail.com>
2018-07-12 21:08:13 -0400

    filtering

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,57 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from "faker";
-import getFilteredUsers from "../server";
-import FilterUsers from "../server/index";
+import filterUsers from "../server/index";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       search: "",
-      data: []
+      data: [],
+      display: []
     };
     this.updateSearch = this.updateSearch.bind(this);
-    this.databaseInfo = this.databaseInfo.bind(this);
+    this.dataMatches = this.dataMatches.bind(this);
+    this.dataSearch = this.dataSearch.bind(this);
   }
 
-  databaseInfo(dataFromChild) {
-    console.log(dataFromChild);
-    
-    // this.setState({
-    //   data: dataFromChild
-    // });
+  componentDidMount() {
+    // console.log(filterUsers());
+    const allData = filterUsers();
+    this.setState({
+      data: allData
+    });
+    let dataToSearch = this.state.data.filter(user => {
+      return user.firstname.toLowerCase().indexOf(this.state.search) !== -1;
+    });
   }
+
+  // filtering() {
+  //   console.log(dataToSearch);
+    
+  //   let dataToSearch = this.state.data.filter(user => {
+  //     return user.firstname.toLowerCase().indexOf(this.state.search) !== -1;
+  //   });
+  //   this.dataMatches(dataToSearch);
+  // }
+
+  dataMatches(userData) {
+    console.log(userData);
+    let dataToSearch = this.state.data.filter(user => {
+      return user.firstname.toLowerCase().indexOf(this.state.search) !== -1;
+    });
+
+  }
+  // filter as they type
+  // render the matches - hold them in sttate
+  // on click render on the page, stave to state
+
+  dataSearch(dataToSearch) {
+    const dataFiltered = dataToSearch.map((data) => {
+      return (data.firstname, data.lastname)
+      // const firstName = data.firstname;
+      // const lastName =  data.lastname;
+      console.log(data.firstname);                      
+    })
+
+    this.dataMatches(dataFiltered);
+    }
 
   // get the value of the user keystroke, hold in state
   updateSearch(e) {
     const userInput = e.target.value.substr(0, 30);
+
+    let dataToSearch = this.state.data.filter(user => {
+      return user.firstname.toLowerCase().indexOf(this.state.search) !== -1;
+    });    
     this.setState({
       search: userInput
     });
+    this.dataSearch(dataToSearch);
   }
 
   render() {
-    
-    // let filteredData = this.props.data.filter(
+    // let filteredData = this.state.data.filter(
     //   (user) => {
-    //     return user.firstname.indexOf(this.state.search) !== -1;
+    //     return user.firstname.toLowerCase().indexOf(this.state.search) !== -1;
     //   }
     // );
-    return <div>
+
+    return (
+      <div>
         <h1>Search It!</h1>
         <label>Search a name!</label>
+        <input
+          type="text"
+          value={this.state.search}
+          onChange={this.updateSearch}
+        />
+        <ul />
         {/* i did the filtering here instead? */}
         {/* <ul>
-          {this.props.data.map((`${data.first} ${data.last}`) => {
-            return <FilterUsers users={this.databaseInfo} key={users.id} />;
+          {filteredData.map((data, i) => {
+            console.log(data);                        
+            return <li key={i}>
+                {data.firstname} {data.lastname}
+              </li>;
           })}
         </ul> */}
-
-        <FilterUsers users={this.databaseInfo} />
-        <input type="text" value={this.state.search} onChange={this.updateSearch} />
-        <ul />
-      </div>;
+      </div>
+    );
   }
 }
 
--- dev/server/index.js
+++ dev/server/index.js
@@ -1,38 +1,22 @@
-import faker from 'faker';
-import React from "react";
+import faker from "faker";
 
 const createUser = () => {
   return {
     firstname: faker.name.firstName(),
     lastname: faker.name.lastName()
-  }
-}
+  };
+};
+
 // Try changing this to test your input!
-const USER_COUNT = 10
+const USER_COUNT = 100;
 
 const users = Array(USER_COUNT)
-  .fill('')
-  .map(x => createUser())
+  .fill("")
+  .map(x => createUser());
 
 // This method is for you to edit, create the filtering however you deem necessary.
-const FilterUsers = (props) => {
+const filterUsers = () => {
+  return users;
+};
 
-  // let usersArray = [];
-
-// for(let i = 0; i < users[i].length; i++) {
-  //   console.log(users[i]);
-  //   let usersObject = {};
-    
-  //   usersObject = {
-  //     firstname: users[i].firstname,
-  //     lastname: users[i].lastname
-  //   };
-  //     usersArray.push(usersObject);
-  //   }
-
-    // props.users(usersArray)
-  props.users(users)
-  return null
-}
-
-export default FilterUsers
+export default filterUsers;
